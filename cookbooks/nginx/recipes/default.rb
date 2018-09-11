20ef6eb1a6a947b1351693941fe910c789e39a2b
Aliaksandr Kavaleu <alexanderkovalev@tut.by>
2018-09-11 20:10:33 +0300

    remove comments nginx recipe

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -12,34 +12,9 @@ directory "/var/www/#{node['nginx']['dest']}" do
   action :create
 end
 
-#content = data_bag_item('landing', 'simple')
-
-#template "/var/www/#{node['nginx']['dest']}/index.html" do   
-#  source "index.html.erb"
-#  mode "0644"
-#  action :create
-#  variables( ct: content )
-#  notifies :reload, "service[nginx]"
-#end
-
-#ip = ''
-
-#ip: 'search(:node, 'roles:jboss')[0]["network"]["interfaces"]["enp0s8"]["addresses"].detect{|k,v| v[:family] == "inet" }.first' #do |result|
-#ip: search(:node, 'role:jboss', :filter_result => { 'ip' => [0]['network']['interfaces']['enp0s8']['addresses'].detect{|k,v| v[:family] == 'inet'}).first #do |result|
-#  ip = result['ip']
-# ip = node["jboss"]["network"]["interfaces"]["enp0s8"]["addresses"].detect{|k,v| v[:family] == "inet" }.first
-#end
-#ip = Array.new
-
-#search(:node, "role:jboss") do |n|
-#  n["network"]["interfaces"]["enp0s8"]["addresses"].each do |address,value|
-#    ip << address if value.has_key?("broadcast")
-#end
-
 template "/etc/nginx/conf.d/jboss.conf" do   
   source "nginx.conf.erb"
   variables(
-#    ip_address: ip,
     ip_nginx: search(:node, 'roles:nginx')[0]["network"]["interfaces"]["enp0s8"]["addresses"].detect{|k,v| v[:family] == "inet" }.first,
     ip_jboss: search(:node, 'roles:jboss')[0]["network"]["interfaces"]["enp0s8"]["addresses"].detect{|k,v| v[:family] == "inet" }.first,
     port: node['nginx']['port'],
